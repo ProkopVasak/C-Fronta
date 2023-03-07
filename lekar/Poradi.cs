bfe5c72d2dcd2d3f0d1e37db504faccc15263c11
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2023-03-07 18:35:56 +0100

    Add files via upload

---
+++ lekar/Poradi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekar
+{
+    internal class Poradi
+    {
+        private static Queue<int> numbers = new Queue<int>();
+        public static int Customer()
+        {
+            Random random = new Random();
+            int num = int.Parse(random.Next(0, 9).ToString() + random.Next(0, 9).ToString() + random.Next(0, 9).ToString());
+            do { 
+                num = int.Parse(random.Next(0, 9).ToString() + random.Next(0, 9).ToString() + random.Next(0, 9).ToString());
+
+            }while(numbers.Contains(num));
+            numbers.Enqueue(num);
+            //Console.WriteLine("Zapsané číslo = "+ num); //test
+            return num;
+        }
+        public static int CustomerCount()
+        {
+            return numbers.Count;
+        }
+        public static void NextCustomer()
+        {
+            if (CustomerCount() > 0)
+            {
+                
+                if (numbers.Peek() < 100) Console.WriteLine("0" + numbers.Dequeue() as string);
+                else Console.WriteLine(numbers.Dequeue());
+            }
+            else Console.WriteLine("Fronta je prázdná");
+        }
+    }
+}
---
+++ lekar/Program.cs
@@ -0,0 +1,4 @@
+﻿using lekar;
+for (int i = 0; i < 10; i++) Poradi.Customer();
+Poradi.CustomerCount();
+for (int i = 0; i < 11; i++) Poradi.NextCustomer();
